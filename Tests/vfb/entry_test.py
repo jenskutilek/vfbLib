5b7f8a7d63f438764e1952205ec010ff92ebee73
Jens Kutilek <webmail@kutilek.de>
2026-09-08 10:35:16 +0200

    Remove deprecation test

--- Tests/vfb/entry_test.py
+++ Tests/vfb/entry_test.py
@@ -6,13 +6,14 @@ from vfbLib.vfb.vfb import Vfb
 
 
 class VfbTest(TestCase):
-    def test_deprecation(self):
-        vfb = Vfb()
-        e = VfbEntry(parent=vfb)
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            # VfbEntry.decompiled is deprecated
-            e.decompiled
-            assert len(w) == 1
-            assert issubclass(w[-1].category, DeprecationWarning)
-            assert "deprecated" in str(w[-1].message)
+    # def test_deprecation(self):
+    #     vfb = Vfb()
+    #     e = VfbEntry(parent=vfb)
+    #     with warnings.catch_warnings(record=True) as w:
+    #         warnings.simplefilter("always")
+    #         # VfbEntry.decompiled is deprecated
+    #         e.decompiled
+    #         assert len(w) == 1
+    #         assert issubclass(w[-1].category, DeprecationWarning)
+    #         assert "deprecated" in str(w[-1].message)
+    pass
