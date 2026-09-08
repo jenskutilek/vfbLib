83f7b1db4c234774433a3526a56f1e94ad86a8f3
Jens Kutilek <webmail@kutilek.de>
2026-09-08 17:37:59 +0200

    Remove Python 3.10 args

--- src/vfbLib/parsers/text.py
+++ src/vfbLib/parsers/text.py
@@ -21,8 +21,7 @@ class NameRecordsParser(BaseParser):
                     char = chr(c)
                     # Fix platform-specific encodings for Mac
                     if platID == 1 and encID == 0:
-                        # TODO: Remove default arguments when Python < 3.11 is dropped
-                        char = c.to_bytes(length=1, byteorder="big").decode("macroman")
+                        char = c.to_bytes().decode("macroman")
                 except ValueError:
                     char = "\ufeff"
                 name += char
