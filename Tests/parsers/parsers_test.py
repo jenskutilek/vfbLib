7c7a94f93687f00737dc3e27fba8409e06a63a48
Jens Kutilek <webmail@kutilek.de>
2026-09-10 13:38:39 +0200

    Add test for string encoding in GlyphEncodingParser

--- Tests/parsers/parsers_test.py
+++ Tests/parsers/parsers_test.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from vfbLib.parsers.base import (
     EncodedValueListParser,
+    GlyphEncodingParser,
     OpenTypeKerningClassFlagsParser,
     OpenTypeMetricsClassFlagsParser,
 )
@@ -61,3 +62,11 @@ class OpenTypeClassFlagsParserTest(TestCase):
 
         result = OpenTypeMetricsClassFlagsParser().parse_hex(data)
         assert result == expected
+
+    def test_glyph_encoding_outside_ascii(self):
+            data = "01 00 61 ae" # 1, a®
+            assert GlyphEncodingParser().parse_hex(data) == (1, "a®")
+
+    def test_glyph_encoding_outside_ascii_2(self):
+        data = "00 00 73 70 61 63 65 8b" # 0, space‹
+        assert GlyphEncodingParser().parse_hex(data) == (0, "space‹")
